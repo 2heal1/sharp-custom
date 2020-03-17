e6a045491b18f06e888353f09f37a541c1f6e3df
2heal1 <TwoHeal@163.com>
2020-03-17 10:34:41 +0800

    feature:初始化布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+//用于适配rem
+import "lib-flexible/flexible";
+import { NumberKeyboard, Button, Field } from "vant";
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.use(NumberKeyboard);
+Vue.use(Button);
+Vue.use(Field);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          // 把px单位换算成rem单位
+          require("postcss-pxtorem")({
+            rootValue: 75, // 换算的基数(设计图750的根字体为32)
+            selectorBlackList: [".van"], // 要忽略的选择器并保留为px。
+            propList: ["*"], //可以从px更改为rem的属性。
+            minPixelValue: 2 // 设置要替换的最小像素值。
+          })
+        ]
+      }
+    }
+  },
+  chainWebpack: config => {
+    // 自定义路径
+    config.resolve.alias.set("@", path.join(__dirname, "src"));
+  }
+};
