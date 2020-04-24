ae1c3efd7f98dfb65ff1d1fe6b03995a11f908f8
2heal1 <TwoHeal@163.com>
2020-04-24 13:07:56 +0800

    feat:增加排序

--- src/pages/Classification/ClassProductList.vue
+++ src/pages/Classification/ClassProductList.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="classProductListView">
-    <van-tabbar
-      v-model="active"
-      :fixed="false"
-    >
+    <van-tabbar v-model="active" :fixed="false" @change="changeSort">
       <van-tabbar-item>综合</van-tabbar-item>
       <van-tabbar-item>销量</van-tabbar-item>
       <van-tabbar-item>上新</van-tabbar-item>
-      <van-tabbar-item>价格<i class="iconfont icon-arrow-down" /></van-tabbar-item>
+      <van-tabbar-item @click="changePrice()"
+        >价格<i
+          :class="{ 'icon-arrow-down': isDown, 'icon-arrow-up': !isDown }"
+          class="iconfont"
+      /></van-tabbar-item>
     </van-tabbar>
     <div class="childView">
       <ProductList :data="data" />
@@ -18,34 +19,83 @@
 <script>
 import ProductList from "@/components/ProductList";
 import productHttp from "@/actions/product";
+import moment from "moment";
 
 export default {
   name: "ClassProductList",
   components: { ProductList },
-  data () {
+  data() {
     return {
       data: [],
+      originData: [],
       active: 0,
+      isactive: false,
+      isDown: true,
     };
   },
   methods: {
-    getProductList () {
+    getProductList() {
       productHttp
-        .getProductList({ productType: this.$route.params.type, detailType: this.$route.query.detailType })
-        .then(res => {
+        .getProductList({
+          productType: this.$route.params.type,
+          detailType: this.$route.query.detailType,
+        })
+        .then((res) => {
           if (res.status === 200) {
+            this.originData = JSON.parse(JSON.stringify(res.data.response));
             this.data = res.data.response;
           }
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     },
-
+    changeSort(type) {
+      type = Number(type);
+      switch (type) {
+        case 0:
+          this.data = this.originData.slice(0);
+          this.isactive = false;
+          break;
+        case 1:
+          this.data = this.originData
+            .slice(0)
+            .sort((a, b) => a.saleVolume - b.saleVolume);
+          this.isactive = false;
+          break;
+        case 2:
+          this.data = this.originData.slice(0).sort(
+            (a, b) =>
+              moment(b.create_time, "YYYY-MM-DD HH:mm:ss")
+                .toDate()
+                .getTime() -
+              moment(a.create_time, "YYYY-MM-DD HH:mm:ss")
+                .toDate()
+                .getTime()
+          );
+          this.isactive = false;
+          break;
+        default:
+          break;
+      }
+    },
+    changePrice() {
+      if (this.isactive) {
+        this.isDown = !this.isDown;
+        this.data = this.originData
+          .slice(0)
+          .sort((a, b) => a.minPrice - b.minPrice);
+      } else {
+        this.isactive = true;
+        this.data = this.originData
+          .slice(0)
+          .sort((a, b) => b.minPrice - a.minPrice);
+      }
+    },
   },
-  mounted () {
+  mounted() {
     this.getProductList();
-  }
+  },
 };
 </script>
 <style lang="less" scoped>
